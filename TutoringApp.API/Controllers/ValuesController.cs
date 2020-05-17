b27424436f84604b8292e7ec79b3ff39d2455d53
taher <taherkhan@live.co.uk>
2020-05-17 17:05:27 +0100

    Angular V9 frontend added

--- TutoringApp.API/Controllers/ValuesController.cs
+++ TutoringApp.API/Controllers/ValuesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TutoringApp.API.Data;
 
 namespace TutoringApp.API.Controllers
 {
@@ -10,19 +12,29 @@ namespace TutoringApp.API.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+
+        }
         // GET api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public async Task<IActionResult> GetValues()
         {
             //throw new Exception("test exception");
-            return new string[] { "value1", "value2" };
+            var values = await _context.Values.ToListAsync();
+
+            return Ok(values);
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public async Task <IActionResult> GetValue(int id)
         {
-            return "value";
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.ID == id);
+
+            return Ok(value);
         }
 
         // POST api/values
